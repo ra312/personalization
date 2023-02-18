361699297ce9652ed064fb553b393afe7fba533a
rauan_akylzhanov <akylzhanov.r@gmail.com>
2023-02-18 07:34:19 +0100

    Basic ML Pipeline Class and tests (#2)
    * Basic ML Pipeline Class and tests
    
        * dev: added added basic MachineLearningPipeline class
        * tests: added tests
    
    * adding basic code and tests
    
        * dev: added ml pipeline tempplate as an abstract class
        * test: added tests for class use
        * ci: check pre-commit, test and coverage
    
    * ci: check coverage immediately

---
+++ tests/test_base_ml_pipeline.py
@@ -0,0 +1,22 @@
+import pytest
+
+from personalization import BaseMachineLearningPipeline
+
+
+class DummyPipeline(BaseMachineLearningPipeline):
+    """a test class to check exceptions are raised
+        if methods are ont overridden in the child class
+
+    Args:
+        BaseMachineLearningPipeline (_type_): _description_
+    """
+
+    def test_method(self):
+        """
+        do not override the required methods
+        """
+
+
+def test_pipeline_abstract_methods():
+    with pytest.raises(TypeError):
+        DummyPipeline()
---
+++ tests/test_more.py
@@ -0,0 +1,40 @@
+import pytest
+
+from personalization import BaseMachineLearningPipeline
+
+
+class DummyPipeline(BaseMachineLearningPipeline):
+    """A test class to check exceptions are not raised
+    if methods are overridden in the child class.
+    """
+
+    def prepare_datasets(self):
+        """Override the required prepare_datasets method."""
+
+    def train(self):
+        """Override the required train method."""
+
+    def __del__(self):
+        """Override the required __del__ method."""
+
+
+class TestDummyPipeline:
+    """Test cases for the DummyPipeline class."""
+
+    def test_prepare_datasets(self):
+        pipeline = DummyPipeline()
+        assert pipeline.prepare_datasets() is None
+
+
+class TestBaseMachineLearningPipeline:
+    """Test cases for the BaseMachineLearningPipeline class."""
+
+    def test_init(self):
+        with pytest.raises(TypeError):
+            BaseMachineLearningPipeline()
+
+    def test_set_model(self):
+
+        pipeline = DummyPipeline()
+        pipeline.model = "test_model"
+        assert pipeline.model == "test_model"
